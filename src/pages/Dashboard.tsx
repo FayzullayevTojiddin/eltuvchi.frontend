5664180ac90171407ab3619fe169123ec5e82908
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2025-10-05 09:06:01 +0500

    change

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -49,7 +49,7 @@ const Dashboard = () => {
 
 
     useEffect(() => {
-        if (!initData) return setLoading(false); // initData bo'lmasa ishlamasin
+        if (initData.length === 0) return setLoading(false); // initData bo'lmasa ishlamasin
 
         try {
             setLoading(true);
